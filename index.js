a42018dfa55f89346215809bc7c9a56583d63406
Chris Aquino <chris@radishmouse.com>
2020-12-04 15:18:43 +0000

    needs fixing!

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ function main() {
     
     const statesWithAHouston = db.statesWithCity('houston');
     console.log('\n\nThere is a Houston in these states:')
-    for (let st of statesWithAHouston) {
+    for (let st of statesWithAnAtlanta) {
         console.log(st);
     }
     
     console.log('\n\n============================');
 
-    const statesWithATampa = db.statesWithCity('tampa');
+    const statesWithATampa = statesWithCity('tampa');
     console.log('\n\nThere is a Tampa in these states:')
-    for (let st of statesWithATampa) {
+    for (st of statesWithATampa) {
         console.log(st);
     }
 
 }
 
-main();
+// main();
