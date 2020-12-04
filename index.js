a99424a963d4ee7cb5f951f188532befd67c119f
Chris Aquino <chris@radishmouse.com>
2020-12-04 15:10:49 +0000

    finished version

---
+++ index.js
@@ -0,0 +1,29 @@
+const db = require('./db');
+
+function main() {
+
+    const statesWithAnAtlanta = db.statesWithCity('atlanta');
+    console.log('\n\nThere is an Atlanta in these states:')
+    for (let st of statesWithAnAtlanta) {
+        console.log(st);
+    }
+    
+    console.log('\n\n============================');
+    
+    const statesWithAHouston = db.statesWithCity('houston');
+    console.log('\n\nThere is a Houston in these states:')
+    for (let st of statesWithAHouston) {
+        console.log(st);
+    }
+    
+    console.log('\n\n============================');
+
+    const statesWithATampa = db.statesWithCity('tampa');
+    console.log('\n\nThere is a Tampa in these states:')
+    for (let st of statesWithATampa) {
+        console.log(st);
+    }
+
+}
+
+main();
